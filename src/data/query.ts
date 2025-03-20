a8cf12253d0fab37d358d84627be9b98b5c61dc3
Paul Adebiyi <padebiyi@renmoney.com>
2025-03-20 20:29:19 +0000

    Remove unused query parameters and clean up Medusa API header configuration

--- src/data/query.ts
+++ src/data/query.ts
@@ -1,17 +1,11 @@
 import { BACKEND_URL } from "@/lib/medusa";
 import { ProductDetailResponse, ProductsResponse } from "../types/types";
 
-// const queryParams = new URLSearchParams({
-//   expand: "variants,variants.prices",
-//   currency_code: "gbp",
-//   limit: "10",
-// });
-
 const medusaHeader = {
   "Content-Type": "application/json",
   "x-publishable-api-key":
     process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY ||
-    "pk_01H9KX7Z8P2Q9M4R5T6W8Y0N2d",
+    "",
 };
 
 const url = `${BACKEND_URL}/store/products`;
